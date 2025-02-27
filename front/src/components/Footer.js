45b6094e418a93394ebd15d7b92817f3dc063dc7
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2025-02-27 12:47:40 -0600

    Update Footer.js

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -4,25 +4,25 @@ const Footer = () =>
     <div>
       <div className="flex justify-evenly items-center h-10 bg-gradient-to-r from-zinc-950 to-slate-700 border-t-2 border-t-black">
         <div>
-          <a href="http://192.168.1.77:3001/standings"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Standings</button></a>
+          <a href="https://archives-prod-88c3247d4963.herokuapp.com/standings"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Standings</button></a>
         </div>
         <div>
             <p>|</p>
         </div>
         <div>
-          <a href="http://192.168.1.77:3001/rosters"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Rosters</button></a>
+          <a href="https://archives-prod-88c3247d4963.herokuapp.com/rosters"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Rosters</button></a>
         </div>
         <div>
             <p>|</p>
         </div>
         <div>
-          <a href="http://192.168.1.77:3001/drafts"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Drafts</button></a>
+          <a href="https://archives-prod-88c3247d4963.herokuapp.com/drafts"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Drafts</button></a>
         </div>
         <div>
             <p>|</p>
         </div>
         <div>
-          <a href="http://192.168.1.77:3001/champions"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Champions</button></a>
+          <a href="https://archives-prod-88c3247d4963.herokuapp.com/champions"><button className='w-full text-md font-poppins font-bold p-1 active:bg-sky-700'>Champions</button></a>
         </div>
       </div>
     </div>
