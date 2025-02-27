338435fc956f3bd6d9dff7fb8ce97d9585a3a6ca
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2025-02-27 13:21:48 -0600

    push

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
           <p className='p-1 2xl:text-2xl font-light'>This archive is not just a record of champions, but a testament to our loyalty, camaraderie, and shared obsession with football.</p>
           <p className='p-1 2xl:text-2xl font-light'>Whether you're here to poke fun at Caleb, relive your own glory days, or to remind Tony that his reign as 2024 champ was a fluke, I hope these archives bring a smile to your face.</p>
         </div>
-        <div className="flex flex-col w-full items-center mt-6 h-96">
-          <img className='w-full max-h-96 p-2 md:w-4/12 lg:w-2/12 xl:w-2/12 2xl:w-2/12 sm:w-6/12' src={ tony } alt='our champion'/>
+        <div className="flex flex-col w-full items-center mt-6 mb-6">
+          <img className='w-full max-h-96 2xl:max-h-[500px] p-2 md:w-4/12 lg:w-4/12 xl:w-4/12 2xl:w-3/12 sm:w-6/12' src={ tony } alt='our champion'/>
         </div>
         <div className="w-10/12 md:w-8/12 lg:w-8/12 xl:w-8/12 2xl:w-8/12 text-center">
           <p className='p-1 2xl:text-2xl font-light'>Here’s to { new Date().getFullYear() - 2014 } years of friendship, football, and the fantasy league that refuses to die. May the best manager win!</p>
